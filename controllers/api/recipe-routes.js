7d3434d79db40b5e0f7f523c0e08789e9e0ebff7
davidbelleza <davidbelleza@gmail.com>
2020-08-27 18:55:09 -0500

    add comment and recipe route

---
+++ controllers/api/recipe-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Recipe } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, (req, res) => {
+    // expects {title: 'Title', content: 'Content', user_id: 1}
+    Recipe.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// update by user_id
+router.put('/:id', withAuth, (req, res) => {
+    Recipe.update(req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// delete by user_id
+router.delete('/:id', withAuth, (req, res) => {
+    Recipe.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+module.exports = router;
