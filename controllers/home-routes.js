0a31532ada682bdb0bcd477030cdc00496abd979
davidbelleza <davidbelleza@gmail.com>
2020-08-30 00:34:12 -0500

    add single-recipe handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,4 +35,55 @@ router.get('/', (req, res) => {
         });
 });
 
+// get single post
+router.get('/recipe/:id', (req,res) => {
+  Recipe.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'ingredients',
+        'recipe_steps',
+        'category',
+        'image_url',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'user_id', 'recipe_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+  })
+  .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const recipe = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-recipe', {
+        recipe,
+        loggedIn: req.session.loggedIn
+      });
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+})
+
 module.exports = router;
