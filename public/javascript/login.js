a877fa867a8f64e2473f063e0635c5aae3aa72a8
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-31 15:15:33 -0500

    fix login route

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -24,10 +24,10 @@ async function signupFormHandler(event) {
 }
 
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
+    event.preventDefault();  
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
+
   
     if (username && password) {
       const response = await fetch('/api/users/login', {
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
