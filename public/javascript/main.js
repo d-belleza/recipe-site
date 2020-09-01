4ff7cea6c7e1a2a7600b08049648d3035c59978f
Sylvia <sylviaprabudy@gmail.com>
2020-09-01 18:24:25 -0500

    add jumbotron

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -188,6 +188,21 @@
 
     };
 
+    function deferVideo() {
+
+        //defer html5 video loading
+      $("video source").each(function() {
+        var sourceFile = $(this).attr("data-src");
+        $(this).attr("src", sourceFile);
+        var video = this.parentElement;
+        video.load();
+        // uncomment if video is not autoplay
+        //video.play();
+      });
+    
+    }
+    window.onload = deferVideo;
+    
 
     $(function () {
         mobileMenuOutsideClick();
@@ -196,6 +211,7 @@
         dropdown();
         tabs();
         goToTop();
+        deferVideo();
       
     });
 
